92e4b7f1cec94846c5ee50f5f84f4543586dce1f
SushilMario <sushilmario@gmail.com>
2020-06-26 12:44:59 +0530

    Add home page ejs file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/",
     function(req, res)
     {
-        res.send("HOME PAGE!");
+        res.render("home");
     }
 )
 

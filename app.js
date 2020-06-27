b7500c3c7fbcbdfe356764620f6f78b99d0eb801
SushilMario <sushilmario@gmail.com>
2020-06-27 12:47:02 +0530

    Add semantic ui, style landing page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+//Dependencies
 var express = require("express");
-var app = express();
 
+
+//App set up
+var app = express();
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/",
     function(req, res)

9d921dffe60c4ecfd3ef508969290230acad134b
SushilMario <sushilmario@gmail.com>
2020-06-28 11:32:32 +0530

    Rename home.ejs to landing.ejs, add hamburger menu

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 //Dependencies
-var express = require("express");
+var    express = require("express"),
+    bodyParser = require("body-parser"),
+      mongoose = require("mongoose");
 
+//Mongoose setup
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/pub-lib");
 
 //App set up
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/",
     function(req, res)
     {
-        res.render("home");
+        res.render("landing");
     }
 )
 
